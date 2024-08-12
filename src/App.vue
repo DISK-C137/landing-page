56e94f82b00bc974860c292d5e782f5b33157ee3
DISK-C137 <csd26prime@gmail.com>
2024-08-13 01:51:23 +0700

    Go

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,241 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <div class="bg-image">
+
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow p-3 mb-5 bg-body rounded">
+          <div class="container">
+            
+            <a class="navbar-brand" href="#">WELCOME TO THE ROCK PAGE</a>
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ms-auto">
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      <main>
+
+        <div class="container my-5">
+
+          <section class="my-5">
+
+            <div class="row">
+              <div class="col-6 text-start d-flex align-items-center" id="head1">
+                <div class="text-left">
+                  <p class="head" data-aos="zoom-in">-CREATE BY CHINNAWAT S.</p>
+
+                  <h1 style="font-size: 50px;" data-aos="fade-right">WELCOME TO THE ROCK PAGE</h1>
+                  
+                  <p class="lead" data-aos="zoom-in">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint consequatur animi vitae
+                    minus itaque officiis similique adipisci quos sapiente amet odio, architecto, laborum delectus
+                    voluptatem nemo molestias corrupti at ad porro harum expedita placeat. Laboriosam labore veniam
+                    quisquam delectus et!</p>
+
+                  <p class="lead" data-aos="zoom-in">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint consequatur animi vitae
+                    minus itaque officiis similique adipisci quos sapiente amet odio, architecto, laborum delectus
+                    voluptatem nemo molestias corrupti at ad porro harum expedita placeat. Laboriosam labore veniam
+                    quisquam delectus et!</p>
+
+                  <a href="#" class="btn btn-dark btn-lg" data-aos="zoom-in">Get Started</a>
+                </div>
+              </div>
+
+              <div class="col-6 d-flex justify-content-center align-items-center" data-aos="fade-left">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img src="@/assets/rocklogo1.png" class="img-fluid">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="@/assets/rocklogo2.png" class="img-fluid">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="@/assets/rocklogo3.png" class="img-fluid">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </section>
+
+        </div>
+
+        <div class="container-fluid" id="container1">
+          <div class="container">
+
+            <h1 class="text-center pb-3" style="color: azure;" data-aos="fade-up">BEST OF VOCAL</h1>
+
+            <div class="card-group" data-aos="zoom-in">
+              <div class="card">
+                <img src="@/assets/v1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Robert Plant</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis aliquam sed
+                    dolorem, magnam quod vero illum, perferendis deserunt dignissimos nemo maxime suscipit architecto
+                    delectus unde recusandae laudantium neque tenetur quam!</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+              </div>
+              <div class="card">
+                <img src="@/assets/v2.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Freddie Mercury</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel blanditiis quos,
+                    possimus distinctio labore nihil magnam totam soluta, reiciendis illo adipisci unde. Nobis cumque
+                    unde deleniti ratione quia quas doloribus.</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+              </div>
+              <div class="card">
+                <img src="@/assets/v3.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Ozzy Osbourne</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, harum! Cupiditate
+                    deleniti inventore delectus dolorem culpa voluptas magnam eum facere fugiat cum incidunt, aspernatur
+                    sequi autem dolor nisi temporibus eaque.
+                  </p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+              </div>
+              <div class="card">
+                <img src="@/assets/v4.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Ronnie James Dio</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore molestiae
+                    soluta magni modi, nam magnam blanditiis. Excepturi quo voluptates possimus maiores numquam velit,
+                    aliquam repudiandae ea sint minus praesentium laborum?</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="container my-5">
+          <section class="my-5">
+
+            <h1 class="pb-3" data-aos="fade-up">THE BEST STORY</h1>
+
+            <div class="card mb-3" data-aos="fade-right" style="max-width: auto;">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="@/assets/s1.jpg" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Stairway to Heaven</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati omnis at
+                      dicta voluptas consequatur quam quasi neque fugiat repellendus sint, dolorum assumenda
+                      consequuntur fuga id quibusdam, rem error, eveniet alias. Iusto quod eius eum laborum nobis nulla
+                      ratione! Illum nobis fuga natus alias doloremque reprehenderit ab incidunt. Accusamus, ullam
+                      aspernatur.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mb-3" data-aos="fade-right" style="max-width: auto;">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="@/assets/s2.jpg" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Black Sabbath (album)</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati omnis at
+                      dicta voluptas consequatur quam quasi neque fugiat repellendus sint, dolorum assumenda
+                      consequuntur fuga id quibusdam, rem error, eveniet alias. Iusto quod eius eum laborum nobis nulla
+                      ratione! Illum nobis fuga natus alias doloremque reprehenderit ab incidunt. Accusamus, ullam
+                      aspernatur.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mb-3" data-aos="fade-right" style="max-width: auto;">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="@/assets/s3.jpg" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Daft Punk - Robot Rock</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati omnis at
+                      dicta voluptas consequatur quam quasi neque fugiat repellendus sint, dolorum assumenda
+                      consequuntur fuga id quibusdam, rem error, eveniet alias. Iusto quod eius eum laborum nobis nulla
+                      ratione! Illum nobis fuga natus alias doloremque reprehenderit ab incidunt. Accusamus, ullam
+                      aspernatur.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </section>
+
+        </div>
+
+      </main>
+
+    </div>
+
+  </div>
+
+  <footer class="bg-dark text-white py-5" data-aos="fade-up">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h5>About Us</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua.</p>
+        </div>
+        <div class="col-md-4">
+          <h5>Quick Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-white">Home</a></li>
+            <li><a href="#" class="text-white">About</a></li>
+            <li><a href="#" class="text-white">Contact</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <h5>Contact Us</h5>
+          <p>1234 Street Name, City, State, 12345</p>
+          <p>Email: <a href="mailto:info@example.com" class="text-white">info@example.com</a></p>
+          <p>Phone: (123) 456-7890</p>
+        </div>
+      </div>
+      <div class="text-center mt-4">
+        <p class="mb-0">© 2024 Your Company. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'App'
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped src="./style.css"></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
-createApp(App).mount('#app')
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // นำเข้า CSS ของ AOS
+
+const app = createApp(App);
+
+app.mount('#app');
+
+AOS.init({
+    duration: 1000, // ระยะเวลาแอนิเมชัน
+    easing: 'ease-in-out', // รูปแบบการเปลี่ยนแปลง
+  });
